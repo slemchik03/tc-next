1e6b1d046b91db1ef01834d08a5ffe1df3270306
Kompik <32t34>
2022-05-03 23:12:40 +0300

    1.6

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,21 @@
 import { Layout } from '../components/Layout';
+import NextNProgress from "nextjs-progressbar";
 import '../styles/scss/style.scss'
 
 import 'swiper/css';
 import 'swiper/css/navigation';
 
 
-//import Layout from './../components/layout'
-
-
 function MyApp({ Component, pageProps }) {
   return (
        <Layout>
+         <NextNProgress
+          color="red"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={3}
+          showOnShallow={true}
+         />
          <Component {...pageProps} />
        </Layout>
   );
--- pages/catalog.jsx
+++ pages/catalog.jsx
@@ -60,7 +60,7 @@ export default function Catalog({goods, pageCount, currentPage, uri, filters}) {
                       return (
                         <div key={index} className="catalog__item">
                         <div className="catalog__item-img">
-                            <Image width="384px" height="256px" src={element.img} alt="" />
+                            <Image width="384px" height="256px" src={element.image} alt="" />
                         </div>
                         <div className="catalog__item-favorite-btn">
                             <svg xmlns="http://www.w3.org/2000/svg" height="28px" viewBox="0 0 24 24" width="28px" fill="gray"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"></path></svg>
@@ -140,19 +140,11 @@ export async function getServerSideProps(context) {
         const products = productsResponse["products"]
         const pageCount = productsResponse["pages"]
 
-        const goodsList = []
-
-        for (let i = 0; i < products.length; i++) { // ищем каждый товар по slug и добавляем в список товаров
-            const element = products[i]
-            const goodsItem = (await axios.get(`https://trade-group.su/apicatalog?slug=${element["slug"]}`)).data
-            goodsList.push(...goodsItem)
-        } 
-
         const filters = filtersResponse.filter(item => item.is_filtered === "1")
-
+        console.log(products);
         return {
             props: {
-                goods: goodsList,
+                goods: products,
                 pageCount,
                 currentPage: page,
                 uri: context.resolvedUrl,
